7edc80046ae0cd16fed8be79cbd720a422bc56b7
sun000903 <shong@aicom.co.jp>
2026-04-20 15:27:02 +0900

    Add files via upload

---
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,19 @@
+package kadai_018;
+
+public class KatoIchiro_Chapter18 extends Kato_Chapter18{
+	
+	
+	public void setGivenName() {
+		this.givenName = "一郎";
+	}
+
+
+	@Override
+	public void eachIntroduce() {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("好きな食べ物はリンゴです");
+		
+	}
+	
+}
+	
