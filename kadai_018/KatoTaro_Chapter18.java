23014f4f9edcf90556ea3a7a1faaf8cfc7023e42
sun000903 <shong@aicom.co.jp>
2026-04-20 15:07:16 +0900

    Add files via upload

---
+++ kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,24 @@
+package kadai_018;
+
+public class KatoTaro_Chapter18 extends Kato_Chapter18{
+	
+	
+	public void setGivenName() {
+		this.givenName = "太郎";
+	}
+	
+	
+	
+	//kato.setGivenName("太郎");
+
+
+	@Override
+	public void eachIntroduce() {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("私はJavaが得意です");
+		
+		
+		
+	}
+
+}
