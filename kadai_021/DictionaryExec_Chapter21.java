91961be2a891be4369181ab70081f9081a771f54
sun000903 <shong@aicom.co.jp>
2026-04-21 10:41:25 +0900

    Add files via upload

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,25 @@
+package kadai_021;
+
+
+public class DictionaryExec_Chapter21 {
+	
+	public static void main(String[] args) {
+		
+		Dictionary_Chapter21 dictionary= new Dictionary_Chapter21();
+	
+	String[] fruits = {"apple","banana","grape","orange"};
+	
+	
+	dictionary.search(fruits[0]);
+	dictionary.search(fruits[1]);
+	dictionary.search(fruits[2]);
+	dictionary.search(fruits[3]);
+	
+	
+	
+	
+	}
+	
+	
+
+}
